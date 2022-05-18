a933d925c1bc973863f1a6ac04906abe334602c0
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-18 10:54:22 +0300

    initializing the application

---
+++ app/auth/views.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, url_for, redirect
+from flask_bcrypt import Bcrypt
+from .forms import RegistrationForm, LoginForm
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('navbar'))
+    return render_template('login.html', form=form)
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
