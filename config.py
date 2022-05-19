fef14ba4a42f95e1a733f651e5b04529e34e5124
KamauDev-maker <oscarnjenga@gmail.com>
2022-05-19 09:44:54 +0300

    git push

--- config.py
+++ config.py
@@ -1,16 +1,1 @@
-import os 
 
-class Config:
-    
-    pass 
-
-class ProdConfig(config):
-    pass 
-
-class DevConfig(config):
-    
-      DEBUG = True
-config_options = {
-    'development':DevConfig,
-    'production':ProdConfig
-}      
